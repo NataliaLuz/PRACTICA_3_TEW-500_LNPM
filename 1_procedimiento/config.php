8627ce4ed974935a245f6213378b8385c9e18b16
NataliaLuz <128393063+NataliaLuz@users.noreply.github.com>
2023-04-26 23:03:52 -0800

    Initial commit

---
+++ 1_procedimiento/config.php
@@ -0,0 +1,15 @@
+<?php
+//config.php
+/* Credenciales de la base de datos. Suponiendo que está ejecutando MySQL
+servidor con configuración predeterminada (usuario 'root' sin contraseña) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'Practica_3_LNPM');
+/* Conexión a la base de datos MySQL */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+// Verifica la conexión
+if($link === false){
+die("ERROR: No se pudo conectar. " . mysqli_connect_error());
+}
+?>
